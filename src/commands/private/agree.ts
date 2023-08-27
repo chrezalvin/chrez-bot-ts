f75fa23d185dadcd6654c9d47b01971ab2f34716
chrezalvin <67034165+chrezalvin@users.noreply.github.com>
2023-08-27 19:03:24 +0700

    adding description option to agree & disagree

--- src/commands/private/agree.ts
+++ src/commands/private/agree.ts
@@ -8,13 +8,27 @@ const command: CommandReturnTypes = {
     name: "agree",
     description: "Agrees with you",
     alias: ["agrees", "agreed", "approve"],
-    execute: async (message) => {
-        await message.channel.send(agrees[rngInt(0, agrees.length - 1)]);
+    execute: async (message, args?: string[]) => {
+        if(args){
+            const embed = new MyEmbedBuilder({title: args.join(" "), description: agrees[rngInt(0, agrees.length - 1)]})
+            await message.channel.send({embeds: [embed]});
+        }
+        else
+            await message.channel.send(agrees[rngInt(0, agrees.length - 1)]);
     },
     slash:{
-        slashCommand: new SlashCommandBuilder().setName("agree").setDescription("Agrees with you"),
-        interact: async (interaction) => {        
-            await interaction.reply(agrees[rngInt(0, agrees.length - 1)]);
+        slashCommand: new SlashCommandBuilder()
+            .setName("agree")
+            .setDescription("Agrees with you")
+            .addStringOption(str => str.setDescription("your message for chrezbot to agree with").setRequired(false).setName("description")),
+        interact: async (interaction) => {
+            const description = interaction.options.getString("description", false);
+            if(description){
+                const embed = new MyEmbedBuilder({title: description, description: agrees[rngInt(0, agrees.length - 1)]})
+                await interaction.reply({embeds: [embed]});
+            }
+            else
+                await interaction.reply(agrees[rngInt(0, agrees.length - 1)]);
         }
     }
 };
--- src/commands/private/disagree.ts
+++ src/commands/private/disagree.ts
@@ -8,13 +8,27 @@ const command: CommandReturnTypes = {
     name: "disagree",
     description: "disagrees with you",
     alias: ["reject", "diagreed", "nope", "nah"],
-    execute: async (message) => {
-        await message.channel.send(disagrees[rngInt(0, disagrees.length - 1)]);
+    execute: async (message, args?: string[]) => {
+        if(args){
+            const embed = new MyEmbedBuilder({title: args.join(" "), description: disagrees[rngInt(0, disagrees.length - 1)]})
+            await message.channel.send({embeds: [embed]});
+        }
+        else
+            await message.channel.send(disagrees[rngInt(0, disagrees.length - 1)]);
     },
     slash:{
-        slashCommand: new SlashCommandBuilder().setName("disagree").setDescription("disagrees with you"),
+        slashCommand: new SlashCommandBuilder()
+            .setName("disagree")
+            .setDescription("disagrees with you")
+            .addStringOption(str => str.setDescription("your message for chrezbot to disagree with").setRequired(false).setName("description")),
         interact: async (interaction) => {        
-            await interaction.reply(disagrees[rngInt(0, disagrees.length - 1)]);
+            const description = interaction.options.getString("description", false);
+            if(description){
+                const embed = new MyEmbedBuilder({title: description, description: disagrees[rngInt(0, disagrees.length - 1)]})
+                await interaction.reply({embeds: [embed]});
+            }
+            else
+                await interaction.reply(disagrees[rngInt(0, disagrees.length - 1)]);
         }
     }
 };
