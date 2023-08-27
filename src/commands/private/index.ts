a7b87fd72a6d8a1100913daa707f53e22b841ecd
chrezalvin <67034165+chrezalvin@users.noreply.github.com>
2023-08-27 18:46:22 +0700

    adding new inline commands

--- src/commands/private/index.ts
+++ src/commands/private/index.ts
@@ -2,11 +2,15 @@ import { CommandReturnTypes } from "@typings/customTypes";
 import bulkDelete from "./bulkdelete";
 import why from "./why";
 import laugh from "./laugh";
+import agree from "./agree";
+import disagree from "./disagree";
 
 export const commands: CommandReturnTypes[] = [
     bulkDelete,
     why,
-    laugh
+    laugh,
+    agree,
+    disagree
 ].filter(command => !command.unavailable);
 
 export default commands;
