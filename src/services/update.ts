b251fb190f0ac54b15c843d57aaf462210ff1ef4
chrezalvin <67034165+chrezalvin@users.noreply.github.com>
2024-12-18 16:05:32 +0700

    database class update
    - created class ServiceFileSupabase as supabase wrapper
    
    - changed model of event interface

--- src/services/update.ts
+++ src/services/update.ts
@@ -1,6 +1,45 @@
 import {ServiceSupabase} from "@library";
 import { Update, isUpdate } from "@models";
 
+class Testing<
+        Type extends Object, 
+        Key extends Extract<keyof Type, string>,
+        OmitKey extends keyof Type = never
+    >{
+    data: Type;
+
+    constructor(data: Type){
+        this.data = data;
+    }
+
+    addData(newData: Omit<Type, Key | OmitKey>): Type{
+        return {} as Type;
+    }
+
+    updateData(key: Key, newData: Partial<Omit<Type, OmitKey>>): Type{
+        return {} as Type;
+    }
+
+    deleteData(key: Key): void{
+        return;
+    }
+
+    getData(key: Key): Type{
+        return {} as Type;
+    }
+}
+
+interface MyType{
+    a: string;
+    b: number;
+    c: number;
+    d: string;
+}
+
+new Testing<MyType, "a">( {a: "a", b: 1, c: 2, d: "d"} );
+
+type MyTypeOmitted = Omit<MyType, "a" | "b">;
+
 export class UpdateService{
     protected static dbName = "updates";
 
