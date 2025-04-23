d72b57191d3b836ef859303963d12cf76b18e3c7
chrezalvin <67034165+chrezalvin@users.noreply.github.com>
2025-04-23 09:49:30 +0700

    added more timeout duration on detection

--- src/shared/YoloService.ts
+++ src/shared/YoloService.ts
@@ -1,3 +1,7 @@
 import { YOLOService } from "@library";
+import { MODE } from "@config";
 
-export const yoloService = new YOLOService();
+export const yoloService = new YOLOService({
+    // production is termux-based and takes much longer to do a detection
+    timeout: MODE === "development" ? 5000 : 30000,
+});
